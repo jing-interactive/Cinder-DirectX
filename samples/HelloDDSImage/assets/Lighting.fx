3d8547b1ffbfda2a9c4628f6f356ce5b42874f95
vinjn <vinjn.z@gmail.com>
2012-10-08 18:35:57 +0800

    add #sample# HelloDDSImage, to test DDS loading

---
+++ samples/HelloDDSImage/assets/Lighting.fx
@@ -0,0 +1,125 @@
+//=============================================================================
+// Lighting.fx by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Transforms and lights geometry.
+//=============================================================================
+
+#include "LightHelper.fx"
+ 
+cbuffer cbPerFrame
+{
+	DirectionalLight gDirLight;
+	PointLight gPointLight;
+	SpotLight gSpotLight;
+	float3 gEyePosW;
+};
+
+Texture2D gDiffuseMap;
+
+SamplerState samLinear
+{
+	Filter = MIN_MAG_MIP_LINEAR;
+	AddressU = WRAP;
+	AddressV = WRAP;
+};
+
+RasterizerState CullBack
+{
+//	FILLMODE = WIREFRAME;
+	CullMode = FRONT;
+};
+
+cbuffer cbPerObject
+{
+	float4x4 gWorld;
+	//float4x4 gWorldInvTranspose;
+	float4x4 gWorldViewProj;
+	Material gMaterial;
+};
+
+struct VertexIn
+{
+	float3 PosL    : POSITION;
+	float3 NormalL : NORMAL;
+	float2 Tex		: TEXCOORD;
+};
+
+struct VertexOut
+{
+	float4 PosH    : SV_POSITION;
+    float3 PosW    : POSITION;
+    float3 NormalW : NORMAL;
+    float2 Tex		: TEXCOORD;
+};
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout;
+	
+	// Transform to world space space.
+	vout.PosW    = mul(float4(vin.PosL, 1.0f), gWorld).xyz;
+	//vout.NormalW = mul(vin.NormalL, (float3x3)gWorldInvTranspose);
+	vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
+		
+	// Transform to homogeneous clip space.
+	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
+	
+	vout.Tex = vin.Tex;
+	
+	return vout;
+}
+  
+float4 PS(VertexOut pin) : SV_Target
+{
+	float4 texColor = gDiffuseMap.Sample( samLinear, pin.Tex );
+	// Interpolating normal can unnormalize it, so normalize it.
+    pin.NormalW = normalize(pin.NormalW);
+
+	float3 toEyeW = normalize(gEyePosW - pin.PosW);
+
+	// Start with a sum of zero. 
+	float4 ambient = float4(0.2f, 0.2f, 0.2f, 0.0f);
+	float4 diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+	float4 spec    = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// Sum the light contribution from each light source.
+	float4 A, D, S;
+#if 1
+	ComputeDirectionalLight(gMaterial, gDirLight, pin.NormalW, toEyeW, A, D, S);
+	ambient += A;  
+	diffuse += D;
+	spec    += S;
+#endif
+#if 0
+	ComputePointLight(gMaterial, gPointLight, pin.PosW, pin.NormalW, toEyeW, A, D, S);
+	ambient += A;
+	diffuse += D;
+	spec    += S;
+#endif
+#if 0
+	ComputeSpotLight(gMaterial, gSpotLight, pin.PosW, pin.NormalW, toEyeW, A, D, S);
+	ambient += A;
+	diffuse += D;
+	spec    += S;
+#endif   
+	float4 litColor = texColor*(ambient + diffuse)/* + spec*/;
+
+	// Common to take alpha from diffuse material.
+	litColor.a = gMaterial.Diffuse.a * texColor.a;
+
+    return litColor;
+}
+
+technique11 LightTech
+{
+    pass P0
+    {
+		SetRasterizerState( CullBack ); 
+        SetVertexShader( CompileShader( vs_4_0, VS() ) );
+		SetGeometryShader( NULL );
+        SetPixelShader( CompileShader( ps_4_0, PS() ) );
+    }
+}
+
+
+
---
+++ samples/HelloDDSImage/src/basicAppDX11.cpp
@@ -0,0 +1,127 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/Utilities.h"
+#include "cinder/ImageIo.h"
+#include "cinder/Arcball.h"
+#include "cinder/ObjLoader.h"
+
+#include "dx11/RendererDx11.h"
+#include "dx11/VertexTypes.h"
+#include "dx11/Vbo.h"
+#include "dx11/LightHelper.h"
+#include "dx11/Texture.h"
+#include "dx11/ImageSourceDds.h"
+
+using namespace ci;
+using namespace ci::app; 
+using namespace std;
+
+class BasicApp : public AppBasic {
+
+public:
+	void setup()
+	{
+		effect = dx11::HlslEffect(loadAsset(L"Lighting.fx"));
+
+		ObjLoader loader(loadAsset("apple.obj"));
+		loader.load(&duck, boost::tribool::true_value);
+		//duck.read(loadAsset("apple.obj"));
+		vboDuck = dx11::VboMesh(duck);
+		vboDuck.createInputLayout(effect);
+
+		try
+		{
+			texDuck = dx11::Texture(loadImage(loadAsset("apple_tex.dds")));
+		}
+		catch (...)
+		{
+			console() << "Failed to load image." <<endl;
+		}
+		
+		// Directional light.
+		mDirLight.Ambient  = Vec4f(0.1f, 0.1f, 0.1f, 1.0f);
+		mDirLight.Diffuse  = Vec4f(0.2f, 0.2f, 0.2f, 1.0f);
+		mDirLight.Specular = Vec4f(0.2f, 0.2f, 0.2f, 1.0f);
+		mDirLight.Direction = Vec3f(0.57735f, -0.57735f, 0.57735f);
+
+		mtrlDuck.Diffuse = Vec4f(0.48f, 0.77f, 0.46f, 1.0f);
+		mtrlDuck.Ambient = Vec4f(0.48f, 0.77f, 0.46f, 1.0f);
+		mtrlDuck.Specular = Vec4f(0.1f, 0.1f, 0.1f, 4.0f);
+
+		mTransform.setToIdentity();
+	}
+
+	void keyDown( KeyEvent event )
+	{
+		if (event.getCode() == KeyEvent::KEY_ESCAPE)
+			quit();
+	}
+
+	void mouseDown( MouseEvent event )
+	{
+		mArcball.mouseDown( event.getPos() );
+	}
+
+	void mouseDrag( MouseEvent event )
+	{
+		mArcball.mouseDrag( event.getPos() );
+	}
+
+	void update()
+	{
+		// animate our little ducky
+		mTransform.setToIdentity();
+		mTransform.scale(3);
+		Matrix44f rot = mArcball.getQuat().toMatrix44();
+		mTransform *= rot;
+
+		mDirLight.Direction.x = 200.0f*cosf( 0.2f*getElapsedSeconds() );
+		mDirLight.Direction.z = 200.0f*sinf( 0.2f*getElapsedSeconds() );
+
+		effect.uniform("gDirLight", &mDirLight);
+		effect.uniform("gEyePosW", mCam.getEyePoint());
+	}
+
+	void draw()
+	{
+		dx11::clear(ColorA(0.5f, 0.5f, 0.5f));
+
+		effect.bind();
+
+		Matrix44f W = mTransform;
+		Matrix44f V = mCam.getModelViewMatrix();
+		Matrix44f P = mCam.getProjectionMatrix();
+		Matrix44f WVP = P*V*W;
+		effect.uniform("gWorld", W);
+		effect.uniform("gWorldViewProj", WVP);
+		effect.uniform("gDiffuseMap", texDuck);
+		effect.uniform("gMaterial", &mtrlDuck);
+
+		dx11::draw(vboDuck);
+	}
+
+	void resize( ResizeEvent event )
+	{
+		mCam.lookAt( Vec3f( 0.0f, 0.0f, 10.0f ), Vec3f::zero());
+		mCam.setPerspective( 90, event.getAspectRatio(), 0.01f, 1000 );
+
+		mArcball.setWindowSize( getWindowSize() );
+		mArcball.setCenter( Vec2f( getWindowWidth() / 2.0f, getWindowHeight() / 2.0f ) );
+		mArcball.setRadius( 150 );
+	}
+
+private: 
+	Arcball	mArcball;
+	dx11::CameraPerspDX	mCam;
+
+	dx11::DirectionalLight mDirLight;
+
+	Matrix44f mTransform;
+	dx11::Material mtrlDuck;
+	TriMesh	duck;
+	dx11::VboMesh	vboDuck;
+	dx11::Texture	texDuck;
+
+	dx11::HlslEffect effect;
+};
+
+CINDER_APP_BASIC( BasicApp, RendererDX11)
